80da2da05ae6145abc852ecca0a9178468bc0fe1
takatori <takatorisatoshi@team-lab.com>
2016-06-27 21:33:35 +0900

    Add P02

---
+++ src/P02.scala
@@ -0,0 +1,18 @@
+package P02
+
+/** (*) Find the last but one element of a list.
+  *
+  *  Example
+  *   scala > penultimate(List(1,1,2,3,5,8))
+  *   res0: Int = 5
+  */
+object P02 {
+  def main(args: Array[String]): Unit = {
+    println(penultimate(List(1,2,3)))
+  }
+
+  def penultimate[T](list: List[T]): T = list match {
+    case x::xs => list.reverse(1)
+    case _ => sys.error("error")
+  }
+}
