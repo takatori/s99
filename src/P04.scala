b7f688ccae73723b4de779e1097a7f3d7c4c6af0
takatori <takatorisatoshi@team-lab.com>
2016-06-28 20:26:41 +0900

    Add P04

---
+++ src/P04.scala
@@ -0,0 +1,16 @@
+/** P04
+  * (*) Find the number of elements of a list.
+  *
+  *  Example
+  *   scala > length(List(1,1,2,3,5,8))
+  *   res0: Int = 6
+  */
+object P04 {
+  def main(args: Array[String]): Unit = {
+    println(length(List(1,1,2,3,5,8)))
+    println(length(List(1)))
+    println(length(List()))
+  }
+
+  def length[T](list: List[T]): Int = list.foldLeft(0)((x, y) => x + 1)
+}
