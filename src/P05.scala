3dd4014927efd3529c3f710ebef1b4dd0a178690
takatori <takatorisatoshi@team-lab.com>
2016-06-30 00:02:50 +0900

    Add P05

---
+++ src/P05.scala
@@ -0,0 +1,20 @@
+package P05
+
+/** P05
+  * (*) Reverse a list.
+  *
+  *  Example
+  *    scala> reverse(List(1,1,2,3,5,8))
+  *    res0: List[Int] = List(8,5,3,2,1,1)
+  */
+object P05 {
+  def main(args: Array[String]): Unit = {
+    println(reverse(List(1,1,2,3,5,8)))
+  }
+
+  def reverse[T](list: List[T]): List[T] = list match {
+    case x::Nil => List(x)
+    case x::xs => reverse(xs):::List(x)
+    case _ => sys.error("")
+  }
+}
