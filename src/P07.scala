2eb664a163013410fa7f880ac0253bc7434a020c
takatori <takatorisatoshi@team-lab.com>
2016-07-01 20:41:44 +0900

    Add P07

---
+++ src/P07.scala
@@ -0,0 +1,20 @@
+package P07
+
+/** P07
+  * (**) Flatten a nested list structure.
+  *
+  *  Example
+  *   scala> flatten(List(List(1, 1), 2, List(3, List(5, 8))))
+  *   res0: List[Any] = List(1, 1, 2, 3, 5, 8)
+  */
+object P07 {
+  def main(args: Array[String]): Unit = {
+    println(flatten(List(List(1,1), 2, List(3, List(5, 8)))))
+  }
+
+  def flatten(list: List[Any]): List[Any] = list flatMap {
+    case ms:List[_] => flatten(ms)
+    case e => List(e)
+  }
+
+}
