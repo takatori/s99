9e5ef3ba3c9e3ddcd4355907020b2401e59a936d
takatori <takatorisatoshi@team-lab.com>
2016-07-04 23:43:40 +0900

    Add P10

---
+++ src/P10.scala
@@ -0,0 +1,25 @@
+package P10
+
+/** P10
+  * (*) Run-length encoding of a list.
+  *  Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E.
+  *
+  *  Example
+  *   scala> encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+  *   res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+  */
+object P10 {
+  def main(args: Array[String]):Unit = {
+    println(encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+  }
+
+  def encode[T](list: List[T]):List[(Int, T)] = {
+
+    list.foldRight(List[(Int, T)]()) { (h,r) =>
+          if (r.isEmpty) List((1,h))
+          else if (r.head._2 != h) (1,h) :: r
+          else (r.head._1 + 1, h) :: r.tail
+    }
+
+  }
+}
