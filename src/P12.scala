f2e060b0b27eba13d4180b9993003b6bddaf12e7
takatori <takatorisatoshi@team-lab.com>
2016-07-06 20:52:20 +0900

    Add P12

---
+++ src/P12.scala
@@ -0,0 +1,18 @@
+package P12
+
+/** P12
+  * (**) Decode a run-length encoded list.
+  * Given a run-length code list generated as specified in problem P10,
+  * construct its uncompressed version.
+  *
+  *  Example
+  *   scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+  *   res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+  */
+object P12 {
+  def main(args: Array[String]):Unit = {
+    println(decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e))))
+  }
+
+  def decode[T](list: List[(Int, T)]): List[T] = list.flatMap(x => List.fill(x._1)(x._2))
+}
