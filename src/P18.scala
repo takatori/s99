ff7960258e7c7d56402d74e99d76a7e067eaa1f5
takatori <takatorisatoshi@team-lab.com>
2016-07-12 19:43:39 +0900

    Add P18

---
+++ src/P18.scala
@@ -0,0 +1,20 @@
+package P18
+
+/** P18
+  * (**) Extract a slice from a list.
+  * Given two indices, I and K, the slice is the list containing
+  * the elements from and including the Ith element up to but not
+  * including the Kth element of the original list. Start counting
+  * the elements with 0.
+  *
+  *  Example
+  *   scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  *   res0: List[Symbol] = List('d, 'e, 'f, 'g)
+  */
+object P18 {
+  def main(args: Array[String]):Unit = {
+    println(slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)))
+  }
+
+  def slice[T](i: Int, k: Int, list: List[T]):List[T] = list.zipWithIndex filter { v => v._2 >= i && v._2 < k } map {_._1}
+}
