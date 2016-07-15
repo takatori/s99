78756885d50f3b36da1b8dd7882d7a40afa2930c
takatori <takatorisatoshi@team-lab.com>
2016-07-15 20:10:14 +0900

    Add P21

---
+++ src/P21.scala
@@ -0,0 +1,23 @@
+package P21
+
+/** P21
+  * (*) Insert an element at a given position into a list.
+  *
+  *  Example
+  *    scala> insertAt('new, 1, List('a, 'b, 'c, 'd))
+  *    res0: List[Symbol] = List('a, 'new, 'b, 'c, 'd)
+  */
+object P21 {
+  def main(args: Array[String]):Unit = {
+    println(insertAt('new, 1, List('a, 'b, 'c, 'd)))
+  }
+
+  def insertAt[T](e: T, n: Int, list: List[T]):List[T] = {
+    if(n > 0) list.head :: insertAt(e, n-1, list.tail)
+    else e :: list
+  }
+
+  def insertAtA[T](e: T, n: Int, list: List[T]):List[T] = {
+    list.splitAt(n) match { case (pre, post) => pre ::: e :: post}
+  }
+}
