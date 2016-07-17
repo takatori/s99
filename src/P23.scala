9c509cbd3454a7bc905bdd868c9ca76ab3c54c99
takatori <takatorisatoshi@team-lab.com>
2016-07-17 14:30:57 +0900

    Add P23

---
+++ src/P23.scala
@@ -0,0 +1,18 @@
+package P23
+
+import scala.util.Random
+
+/** P23
+  * (**) Extract a given number of randomly selected elements from a list.
+  *
+  *  Example
+  *   scala> randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h))
+  *   res0: List[Symbol] = List('e, 'd, 'a)
+  */
+object P23 {
+  def main(args:Array[String]): Unit = {
+    println(randomSelect(3, List('a, 'b, 'c, 'd, 'f, 'g, 'h)))
+  }
+
+  def randomSelect[T](n: Int, list: List[T]): List[T] = Random.shuffle(list).take(3)
+}
