6e9f08ea239b45af39ed495a7cb66256a9468699
takatori <takatorisatoshi@team-lab.com>
2016-07-18 16:29:33 +0900

    Add P24

---
+++ src/P24.scala
@@ -0,0 +1,18 @@
+package P24
+
+import scala.util.Random
+
+/** P24
+  *  Lotto: Draw N different random numbers from the set 1..M.
+  *
+  *   Example
+  *     scala> lotto(6, 49)
+  *     res0: List[Int] = List(23, 1, 17, 33, 21, 37)
+  */
+object P24 {
+  def main(args: Array[String]):Unit = {
+    println(lotto(6, 49))
+  }
+
+  def lotto(n: Int, limit: Int): List[Int] = Random.shuffle((1 to limit).toList).take(n)
+}
