3d468a743b5c3ecf55a6963e60608aaa4dbe5504
takatori <takatorisatoshi@team-lab.com>
2016-07-19 22:33:09 +0900

    Add P25

---
+++ src/P25.scala
@@ -0,0 +1,20 @@
+package P25
+
+/** P25
+  * (*) Generate a random permutation of the elements of a list.
+  * Hint: Use the solution of problem P23.
+  *
+  *  Example
+  *   scala> randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+  *   res0: List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)
+  */
+object P25 {
+  def main(args: Array[String]):Unit = {
+    println(randomPermute(List('a, 'b, 'c, 'd, 'e, 'f)))
+  }
+
+  def randomPermute[T](list: List[T]): List[T] = {
+    import scala.util.Random
+    Random.shuffle(list)
+  }
+}
